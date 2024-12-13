732211a374c3b6cac1eb4a4dbedf65304ea8188a
Gianluca <10934204@itisrossi.vi.it>
2024-12-13 09:34:33 +0100

    Aggiungere i file di progetto.

---
+++ MinHeapNodi/HeapNode.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinHeapNodi
+{
+    internal class HeapNode<T> where T : IComparable<T>
+    {
+        public T Value { get; set; }
+        public HeapNode<T> Parent { get; set; }
+        public HeapNode<T> SxSon { get; set; }
+        public HeapNode<T> DxSon { get; set; }
+
+        public HeapNode(T value)
+        {
+            Value = value;
+            Parent = null;
+            SxSon = null;
+            DxSon = null;
+        }
+    }
+}
---
+++ MinHeapNodi/MinHeap.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinHeapNodi
+{
+    internal class MinHeap<T> where T : IComparable<T>
+    {
+        public HeapNode<T> Root { get; private set; }
+        public int Count { get; private set; }
+
+        public MinHeap()
+        {
+            Count = 0;
+            Root = null;
+        }
+
+        public void Insert(T value)
+        {
+            HeapNode<T> newNode = new HeapNode<T>(value);
+
+            if(Root ==  null)
+            {
+                Root = newNode;
+            }
+            else
+            {
+
+            }
+            
+            Count++;
+        }
+
+        private HeapNode<T> FindNextPosition()
+        {
+            int nextIndex = Count + 1;
+            string binaryPath = Convert.ToString(nextIndex, 2).Substring(1);
+
+            var current = Root;
+            for (int i = 0; i < binaryPath.Length - 1; i++)
+            {
+                current = binaryPath[i] == '0' ? current.SxSon : current.DxSon;
+            }
+
+            return current;
+        }
+    }
+}
